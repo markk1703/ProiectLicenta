c5a1900806ded9fcc3f40b74c73e3619dae46c67
Konyicska Mark <konyicska_mark98@yahoo.com>
2021-03-13 15:26:45 +0200

    Added Licenta

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id()->autoincremenet();
+            $table->string('username')->unique();
+            $table->string('nume')->nullabe(false);
+            $table->string('prenume')->nullable(false);
+            $table->string('email')->unique();
+            $table->string('password')->nullable(false);
+            $table->string('imagine')->default('default.jpg');
+            $table->string('idRol')->default('2');
+            $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable(); //$table->timestamps(); pt toate
+            $table->timestamp('updated_at')->nullable(false);
+            $table->timestamp('created_at')->nullable(false);
+            $table->timestamp('stamp');
+            $table->boolean('isActiv')->default('1');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
